4f6d20ba128701e53576746a4a21761cddd1e6ec
Risheek Yadav <rish88k@Risheeks-MacBook-Air.local>
2026-02-02 23:51:52 +0530

    minio instantiated

--- consumer/docker-consumer.dockerfile
+++ consumer/docker-consumer.dockerfile
@@ -1,9 +1,9 @@
-FROM python:3.9-slim
+FROM python:3.11-slim
 
 WORKDIR /app 
 
 RUN pip install --no-cache-dir boto3 confluent-kafka
 
-COPY . .
+COPY kafka-2-minio.py .
 
 CMD [ "python", "kafka-2-minio.py" ]
--- consumer/kafka-2-minio.py
+++ consumer/kafka-2-minio.py
@@ -5,38 +5,48 @@ from confluent_kafka import Consumer
 from datetime import datetime
 
 
-KAFKA_CONFIG= {
-    'bootstrap.servers': 'kafka:29092',
-    'group.id': 'minio-consumer-group',
-    'auto.offset.reset': 'earliest'
+KAFKA_CONFIG = {
+    'bootstrap.servers': 'kafka:9092',
+    'group.id': 'minio-sink-consumer',
+    'auto.offset.reset': 'earliest',
+    'enable.auto.commit': True,
+    'security.protocol': 'PLAINTEXT',
 }
+
+print(KAFKA_CONFIG)
 
 MINIO_CONFIG= {
     'endpoint_url': 'http://minio:9000',
     'aws_access_key_id': 'admin',
-    'aws_secret_access_key': 'password',
+    'aws_secret_access_key': 'admin123',
     'region_name': 'us-east-1'
 }
 
 
 consumer= Consumer(KAFKA_CONFIG)
-s3_client= boto3.client('s3', **MINIO_CONFIG)
+s3_client = boto3.client("s3", **MINIO_CONFIG)
 bucket_name= 'banker_bucket'
 
 
 def run_consumer():
+    response = s3_client.list_buckets()
+    print(response["Buckets"])
+
     try:
         s3_client.create_bucket(Bucket=bucket_name)
     except Exception:
         pass # Bucket likely already exists
 
-    consumer.subscribe(['banking.banking.transactions'])
+    consumer.subscribe(['banking_dev.public.acc_accounts'])
 
     print('listening for events')
 
     try:
         while True:
             msg= consumer.poll(100.0)
+            if msg is None:
+                continue
+
             if msg.error():
                 print(f"consumer error : {msg.error()}")
                 continue
